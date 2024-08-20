8473c3adb0ccdfd3efabcf26eedb053ea918ef6d
Sereth1 <papakias444@gmail.com>
2024-08-20 08:08:30 +0800

    a

--- other/recursionFib.ts
+++ other/recursionFib.ts
@@ -1,5 +1,9 @@
 function recursiveFib(n: number): number {
-    return n <= 1 ? n : recursiveFib(n - 1) + recursiveFib(n - 2);
+    if (n < 2) {
+        return n
+    }
+
+    return recursiveFib(n - 1) + recursiveFib(n - 2);
 }
 console.log(recursiveFib(10));
 
